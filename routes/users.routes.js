806fe6f436f41af0fd46dd206112ef02d98468ae
Bhaskar Pandey <43948993+s7k11@users.noreply.github.com>
2022-08-03 12:20:32 +0530

    changes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,6 @@
 import express from "express";
-import { deleteUser, getUsers, registerUser, updateUser, userKyc, getUserData, changeUserKyc, login, loginAdmin, registerAdmin } from "../controllers/users.controller";
-import { authorizeJwt } from "../middlewares/auth.middleware";
+import { changeUserKyc, deleteUser, getUserData, getUsers, login, loginAdmin, registerAdmin, registerUser, userKyc } from "../controllers/users.controller";
 let router = express.Router();
-
 
 router.post("/register", registerUser);
 router.post("/login", login);
@@ -11,9 +9,9 @@ router.post("/userKyc/:id", userKyc);
 router.get("/getUserData", getUserData);
 router.get("/getUsers", getUsers);
 
-router.patch("/changeUserKyc", authorizeJwt, changeUserKyc);
+router.patch("/changeUserKyc", changeUserKyc);
 
-router.delete("/deleteById/:id", authorizeJwt, deleteUser);
+router.delete("/deleteById/:id", deleteUser);
 
 //admin =
 router.post("/registerAdmin", registerAdmin);
