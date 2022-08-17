291b94eaa16895c41a431ce851ae66b4dc5a6d45
Bhaskar Pandey <43948993+s7k11@users.noreply.github.com>
2022-08-17 10:45:11 +0530

    merging

--- routes/contactMail.routes.js
+++ routes/contactMail.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { addMail, downloadQrCode, deleteById, getMail } from "../controllers/contactMail.controller";
 import { authorizeJwt } from "../middlewares/auth.middleware";
+
 let router = express.Router();
 
 router.post("/", addMail);
 
 router.get("/get", getMail);
 
-router.delete("/deleteById/:id", authorizeJwt, deleteById); //only for ADMIN
+router.delete("/deleteById/:id", authorizeJwt, deleteById);
 
-router.get("/qrCode/:text", downloadQrCode) // text will be "www.google.com"
-    //url will be http://localhost:4015/mail/qrCode/asdf
+router.get("/qrCode/:text", downloadQrCode);
 export default router;
