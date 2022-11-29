0f5fe39c3f9ad97a99b4e6da7fd4a66f998b87bc
alwinponnanebslon <108860901+alwinponnanebslon@users.noreply.github.com>
2022-11-29 17:47:49 +0530

    asf

--- routes/conversion.routes.js
+++ routes/conversion.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {
-    AddConversion, getConversions
+    AddConversion, getConversions, updateConversation
 } from "../controllers/conversion.controller";
 
 let router = express.Router();
 router.post("/AddConversion", AddConversion);
 router.get("/getConversions", getConversions);
+router.post("/updateConversation/:id", updateConversation);
 export default router;
