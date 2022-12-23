c5256e728c484ccca3bcf2adff1fd38cfdc87620
joel <joel.green@ebslon.com>
2022-12-23 12:49:28 +0530

    added headless back

--- controllers/Campaign.controller.js
+++ controllers/Campaign.controller.js
@@ -70,7 +70,7 @@ export const searchLinkedin = async (req, res, next) => {
 
 
         let options = new chrome.Options();
-        // options.addArguments('--headless');
+        options.addArguments('--headless');
         options.setPageLoadStrategy(PageLoadStrategy.EAGER)
         options.addArguments('--disable-gpu');
         options.addArguments('--window-size=1920,1080');
