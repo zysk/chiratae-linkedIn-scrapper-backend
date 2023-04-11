6e728d780c1bcaf41a77d4013a18ba6ef7b8b864
alwinponnanebslon <108860901+alwinponnanebslon@users.noreply.github.com>
2023-04-11 11:45:51 +0530

    a

--- app.js
+++ app.js
@@ -74,10 +74,10 @@ app.use("/emailSettings", emailSettingsRouter);
 app.use("/customemail", customemailRouter);
 
 app.use(errorHandler);
-const job = schedule.scheduleJob('32 11 * * *', function () {
-    // const job = schedule.scheduleJob('*/10 * * * *', function () {
-    // const job = schedule.scheduleJob('0 0 * * *', function () {
-    // const job = schedule.scheduleJob('0 6,18 * * *', function () {
+// const job = schedule.scheduleJob('32 11 * * *', function () {
+// const job = schedule.scheduleJob('*/10 * * * *', function () {
+// const job = schedule.scheduleJob('0 0 * * *', function () {
+const job = schedule.scheduleJob('0 6,18 * * *', function () {
     // getScheduledCampaignsForToday()
     cronFunc()
     console.log("At 06:00 and 18:00 on every day-of-week from Sunday through Saturday.")
