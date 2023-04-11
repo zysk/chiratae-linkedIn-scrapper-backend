358157a7babfd9a2c5e9cee1ec26b88b429606b8
alwinponnanebslon <108860901+alwinponnanebslon@users.noreply.github.com>
2023-04-11 11:47:45 +0530

    a

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ export const cronFunc = async () => {
  */
 let options = new chrome.Options();
 options.addArguments("no-sandbox")
-// options.addArguments('--headless');
+options.addArguments('--headless');
 options.setPageLoadStrategy(PageLoadStrategy.EAGER)
 options.addArguments('--disable-gpu');
 options.addArguments('--window-size=1920,1080');
