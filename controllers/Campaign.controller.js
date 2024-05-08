e5adf001bce6885705ee70ecfd35afbe2ed0cda8
Arijit <66446364+sahaarijit@users.noreply.github.com>
2024-05-08 08:42:02 +0530

    update: prod test

--- controllers/Campaign.controller.js
+++ controllers/Campaign.controller.js
@@ -144,7 +144,7 @@ export const linkedInLogin = async (req, res, next) => {
 
         let options = new chrome.Options();
         options.addArguments("no-sandbox");
-        options.addArguments('--headless');
+        // options.addArguments('--headless');
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
