e424519563127e957c0ce0a270e6924362729fcb
Arijit <arijit.saha@zysk.tech>
2024-05-27 12:01:26 +0530

    test: email

--- controllers/EmailSettings.controller.js
+++ controllers/EmailSettings.controller.js
@@ -3,10 +3,13 @@ import EmailSettings from "../models/EmailSettings.model";
 export const createNewEmailSettings = async (req, res, next) => {
     try {
         let existsCheck = await EmailSettings.findOne().exec();
+		let obj = req.body;
+        obj.mailUserPassword = Buffer.from(req.body.mailUserPassword, "base64").toString("ascii");
+
         if (existsCheck) {
-            await EmailSettings.findOneAndUpdate({}, req.body).exec();
+            await EmailSettings.findOneAndUpdate({}, obj).exec();
         } else {
-            let newElement = await new EmailSettings(req.body).save();
+            let newElement = await new EmailSettings(obj).save();
         }
         res.status(200).json({ message: "Email Settings updated", success: true });
     } catch (error) {
